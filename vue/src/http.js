65946ebbe66c5beaeef49ad71e75c033dc2177e7
yuhjhj <yuyuyu@qq.com>
2022-11-18 09:09:41 +0800

    X

--- vue/src/http.js
+++ vue/src/http.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 const http = axios.create({
     timeout: 60000,
     baseURL:process.env.VUE_APP_APISERVE,
-    headers: { 'X-Requested-With': 'XMLHttpRequest' }
+    headers: { 
+			'X-Requested-With': 'XMLHttpRequest',
+			'Authorization':'sdsdsdsdsdds'
+		}
 })
 http.interceptors.response.use(res => {
     return res.data
