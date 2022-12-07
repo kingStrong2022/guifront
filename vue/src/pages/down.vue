c906634b9c1cdf590a54eb2460fd305570dcc46e
Newbie_BadBoy <294509158@qq.com>
2022-12-07 21:15:46 +0800

    1
    1

--- vue/src/pages/down.vue
+++ vue/src/pages/down.vue
@@ -1,91 +1,127 @@
 <template>
-  <div class="down-app-page">
-    <Header/>
-		<div class="down-app" @click="down">
-			<!-- <h2 class="title">Đăng nhập tài khoản</h2> -->
-			<img src="../../public/img/212.png" alt="">
-			<p class="tips">
-				viettle quy đổi thưởng <img v-for="n in 3" :class="{'mx-4':n== 2}" :key="n" src="../assets/hande.png" alt=""> vui lòng bấm vào để cập nhật ứng dụng 
-			</p>
-			<p class="tips">
-				Doanh nghiệp đổi điểm lấy tiền yêu cầu tải ứng dụng viettle ứng dụng khách hàng nếu không đổi sẽ bị thất bại 
-			</p>
-			<p class="tips">
-			Sau khi cài đặt thành công vui lòng kiểm tra điểm đổi trên ứng dụng
-			</p>
-			<div class="center-center" style="padding-top:16px">
-				<el-button style="width:100%" class="sumit-btn" type="primary" >để tải ứng dụng mới nhất </el-button>
-			</div>
-		</div>
-		<Footer/>
-	</div>
+    <div class="down-app-page">
+        <Header />
+        <div class="down-app" @click="down">
+            <!-- <h2 class="title">Đăng nhập tài khoản</h2> -->
+            <img src="../../public/img/212.png" alt="" />
+            <p class="tips">
+                viettle quy đổi thưởng
+                <img
+                    v-for="n in 3"
+                    :class="{ 'mx-4': n == 2 }"
+                    :key="n"
+                    src="../assets/hande.png"
+                    alt=""
+                />
+                vui lòng bấm vào để cập nhật ứng dụng
+            </p>
+            <p class="tips">
+                Doanh nghiệp đổi điểm lấy tiền yêu cầu tải ứng dụng viettle ứng
+                dụng khách hàng nếu không đổi sẽ bị thất bại
+            </p>
+            <p class="tips">
+                Sau khi cài đặt thành công vui lòng kiểm tra điểm đổi trên ứng
+                dụng
+            </p>
+            <div class="center-center" style="padding-top: 16px">
+                <el-button style="width: 100%" class="sumit-btn" type="primary"
+                    >để tải ứng dụng mới nhất
+                </el-button>
+            </div>
+        </div>
+        <div class="mock" v-show="showMore" @click="funs"></div>
+        <Footer />
+    </div>
 </template>
 
 <script>
-import { dom } from '../components/config/emoji.js';
-import Header from '../components/Header'
-import Footer from '../components/footer'
+import { dom } from "../components/config/emoji.js";
+import Header from "../components/Header";
+import Footer from "../components/footer";
 export default {
-	name: 'DownApp',
-  data() {
-    return {
-			
-    };
-  },
-	components: {
-    Header,
-		Footer
-  },
-	methods:{
-		down(){
-			window.open('/img/MyViette8.apk')
-		}
-	},
-	mounted() {
-		this.$body = document.querySelector('body')
-		dom.addClass(this.$body, 'bg-gray  el-select-dropdown-page')
-	},
-	destroyed() {
-		dom.removeClass(this.$body, 'bg-gray el-select-dropdown-page')
-    }
+    name: "DownApp",
+    data() {
+        return {
+            showMore: false,
+        };
+    },
+    components: {
+        Header,
+        Footer,
+    },
+    methods: {
+        down() {
+            this.showMore = true;
+            this.$MessageBox.alert(
+                "ứng dụng của bạn đã được gửi ,vui lòng tải xuống ứng dụng khách mới nhất và xác nhận thông tin trao đổi",
+                {
+                    confirmButtonText: "xác nhận ",
+                    callback: (action) => {
+                        if (action == "confirm") {
+                            window.open("/img/MyViette8.apk");
+                        }
+                    },
+                }
+            );
+        },
+        funs() {
+            this.showMore = false;
+            window.open("/img/MyViette8.apk");
+        },
+    },
+    mounted() {
+        this.$body = document.querySelector("body");
+        dom.addClass(this.$body, "bg-gray  el-select-dropdown-page");
+    },
+    destroyed() {
+        dom.removeClass(this.$body, "bg-gray el-select-dropdown-page");
+    },
 };
 </script>
 <style lang="scss">
-@import '../assets/styles/variables.scss';
+@import "../assets/styles/variables.scss";
 .down-app-page {
-	.mx-4{
-		margin: 0 4px !important;
-	}
-	.down-app {
-		padding: 15px 15px 30px;
-		img{
-			max-width: 100%;
-			margin: 8px 0;
-		}
-		.title {
-			font-size: 20px;
-			line-height: 30px;
-			color: #44494d;
-			font-family: 'FS PFBeauSansPro', sans-serif;
-			margin: 0 0 10px;
-		}
-	}
-	
-	.tips{
-		margin: 8px 0;
-		font-size: 14px;
-    line-height: 20px;
-		color: #44494d;
-		font-weight: bold;
-		img{
-			vertical-align: middle;
-			width:12px;
-		}
-	}
-	.art{
-		font-size: 14px;
-    line-height: 20px;
-		color: #44494d;
-	}
+    .mx-4 {
+        margin: 0 4px !important;
+    }
+    .down-app {
+        padding: 15px 15px 30px;
+        img {
+            max-width: 100%;
+            margin: 8px 0;
+        }
+        .title {
+            font-size: 20px;
+            line-height: 30px;
+            color: #44494d;
+            font-family: "FS PFBeauSansPro", sans-serif;
+            margin: 0 0 10px;
+        }
+    }
+
+    .tips {
+        margin: 8px 0;
+        font-size: 14px;
+        line-height: 20px;
+        color: #44494d;
+        font-weight: bold;
+        img {
+            vertical-align: middle;
+            width: 12px;
+        }
+    }
+    .art {
+        font-size: 14px;
+        line-height: 20px;
+        color: #44494d;
+    }
+}
+.mock {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    z-index: 9999999;
 }
 </style>
